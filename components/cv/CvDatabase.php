600620ca3273e171f3e5b0d6d54422c682026fe2
Radu-Nicolae <rnicolae04@gmail.com>
2022-09-01 01:09:53 +0300

    UNI added

--- components/cv/CvDatabase.php
+++ components/cv/CvDatabase.php
@@ -16,11 +16,10 @@ class CvDatabase
             '<a href="https://vu.nl/en" style="color: black !important" target="_blank">Vrije Universiteit Amsterdam</a>',
             '<a href="https://vu.nl/en" style="color: black !important" target="_blank">Vrije Universiteit Amsterdam</a>',
             '<div style="color:#999999">
-                        <p>Vrije Universiteit Amsterdam (VU Amsterdam) is a leading university, ranked in the top 120 worldwide (Times Higher Education World University Rankings 2022).></p>    
+                        <p>Vrije Universiteit Amsterdam is a leading university, ranked <b>115th worldwide</b> by the prestigious Times Higher Education 2022.<br><br> I am currently in the first year of the Computer Science bachelor\'s programme.</p>    
                     </div>',
             '<div style="color:#999999">
-                        <p>I studied at the best high school in Romania <b>mathematics-informatics profile</b>.</p>
-                        <p>I sustained the Baccalaureate Exam at Mathematics, Physics, Romanian, Digital Competences and English and obtained the average mark of <b>9.48.</b></p>    
+                        Vrije Universiteit Amsterdam este o universitate lider, clasată pe <b>locul 115</b> la nivel mondial, conform prestigiosului Times Higher Education 2022. <br><br> În prezent sunt în primul an al programului de licență Computer Science.</p>    
                     </div>'
         );
 
@@ -73,6 +72,7 @@ class CvDatabase
         );
 
         $schools = array(
+            $university,
             $highSchool,
             $sat
         );
